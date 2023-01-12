d6705b5a49e3d4bc5fa2a1426d66ac5e9c4c44cc
rida2015 <ridaeljirari@gmail.com>
2023-01-12 13:25:56 +0100

    ft_print_comb

---
+++ ft_print_comb.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int r;
+	int e;
+	int d;
+
+	r = 0 - 1;
+	while (r < 7)
+	{
+		r++;
+		e = r;
+		while(e < 8)
+		{
+			e++;
+			d = e;
+			while(d < 9)
+			{
+				d++;
+				ft_putchar(r+48);
+				ft_putchar(e+48);
+				ft_putchar(d+48);
+				if (r != 7)
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	ft_print_comb();
+}
